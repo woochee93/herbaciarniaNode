fe7048177ade29f90803855554a5fa72276bae0d
Mateusz Łuczkowski <mateuszluczkowski@gmail.com>
2021-08-05 22:03:42 +0200

    add routes for gallery/ add lightbox in routes

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,35 @@
 var express = require("express");
 var router = express.Router();
-
 router.get("/", function (req, res, next) {
   res.render("gallery/gallery", {
     title: "Galeria - Herbaciarnia Ziołowa - Sobótka",
+  });
+});
+
+router.get("/tea", function (req, res, next) {
+  res.render("gallery/subgallery/galleryTea", {
+    title: "Herbata - Galeria - Herbaciarnia Ziołowa - Sobótka",
+  });
+});
+
+router.get("/interior", function (req, res, next) {
+  res.render("gallery/subgallery/galleryInterior", {
+    title: "Wnętrze - Galeria - Herbaciarnia Ziołowa - Sobótka",
+  });
+});
+router.get("/store", function (req, res, next) {
+  res.render("gallery/subgallery/galleryStore", {
+    title: "Sklep - Galeria - Herbaciarnia Ziołowa - Sobótka",
+  });
+});
+router.get("/events", function (req, res, next) {
+  res.render("gallery/subgallery/galleryEvents", {
+    title: "Wydarzenia - Galeria - Herbaciarnia Ziołowa - Sobótka",
+  });
+});
+router.get("/workshops", function (req, res, next) {
+  res.render("gallery/subgallery/galleryWorkshops", {
+    title: "Warsztaty - Galeria - Herbaciarnia Ziołowa - Sobótka",
   });
 });
 
